9fa8a6739fc3ede3c44495da9d97b8356bad7736
赵成围 <ivorzk@qq.com>
2019-09-04 20:19:10 +0800

    v

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// export default {
-//   install(Vue, options) {
-//     // 获取配置文件
-//     let $config = options.file
-//     console.log($config, '$config')
-//     // 获取环境
-//     let env = options.default || 'dev'
-//     // 混入参数
-//     let mixconfig = {
-//       // 混入通用的配置
-//       ...$config.common || {},
-//       // 混入当前环境配置
-//       ...$config[env] || {}
-//     }
-//     // 注入到Vue实例中
-//     Vue.prototype.$config = mixconfig
-//     // 尝试注入到window当中
-//     if (window) window.$config = mixconfig
-//   }
-// }
-
 module.exports = (api, options) => {
   // api.chainWebpack(webpackConfig => {
   //   // 通过 webpack-chain 修改 webpack 配置
@@ -28,7 +7,26 @@ module.exports = (api, options) => {
 
   })
 
-  // api.registerCommand('test', args => {
-  //   // 注册 `vue-cli-service test`
-  // })
+  // 获取配置文件
+  let $config = require(options.file).default
+
+  console.log($config, '$config')
+  // 获取环境
+  let env = options.default || 'dev'
+  // 混入参数
+  let mixconfig = {
+    // 混入通用的配置
+    ...$config.common || {},
+    // 混入当前环境配置
+    ...$config[env] || {}
+  }
+  // 注入到Vue实例中
+  Vue.prototype.$config = mixconfig
+  // 尝试注入到window当中
+  if (window) window.$config = mixconfig
+}
+
+// api.registerCommand('test', args => {
+//   // 注册 `vue-cli-service test`
+// })
 }
