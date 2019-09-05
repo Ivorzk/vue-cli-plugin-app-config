736413e09a3533540695b9bc81c0014d4ee6c745
赵成围 <ivorzk@qq.com>
2019-09-05 19:00:40 +0800

    v

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 module.exports = (api, options) => {
-  // api.chainWebpack(webpackConfig => {
-  //   // 通过 webpack-chain 修改 webpack 配置
-  // })
-  console.log('进入插件', options)
-  api.configureWebpack(webpackConfig => {
 
+  console.log('进入插件', options)
+
+  // 混入配置文件
+  let mergeConfig = (args) => {
+    // 获取配置文件
+    let $allConfig = require(options.file).default
+
+    console.log($allConfig, '$allConfig', args, 'args')
+
+    // 获取环境
+    let env = args || options.default || 'dev'
+    // 混入参数
+    let $config = {
+      // 混入通用的配置
+      ...$allConfig.common || {},
+      // 混入当前环境配置
+      ...$allConfig[env] || {}
+    }
+
+    // 混入全局的webpack中
+    api.configureWebpack(webpackConfig => {
+      webpackConfig.plugins = [
+        new webpack.DefinePlugin({
+          $config: $config
+        })
+      ]
+    })
+  }
+
+  // 注册命令行
+  api.registerCommand('serve', args => {
+    mergeConfig(args)
   })
 
-  // 获取配置文件
-  let $config = require(options.file).default
+  api.registerCommand('build', args => {
+    mergeConfig(args)
+  })
 
-  console.log($config, '$config')
-  // 获取环境
-  let env = options.default || 'dev'
-  // 混入参数
-  let mixconfig = {
-    // 混入通用的配置
-    ...$config.common || {},
-    // 混入当前环境配置
-    ...$config[env] || {}
-  }
-  // 注入到Vue实例中
-  Vue.prototype.$config = mixconfig
-  // 尝试注入到window当中
-  if (window) window.$config = mixconfig
-}
+  api.registerCommand('test', args => {
+    mergeConfig(args)
+  })
 
-// api.registerCommand('test', args => {
-//   // 注册 `vue-cli-service test`
-// })
 }
